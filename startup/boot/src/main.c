09055af50cceb100b330702e434ed6b1b959747d
Daniel Schultz <d.schultz@phytec.de>
2021-09-25 17:16:16 +0200

    startup: Add boot as minimal prompt
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

---
+++ startup/boot/src/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Phytec Messtechnik GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/gpio.h>
+#include <drivers/uart.h>
+#include <sys/printk.h>
+#include <sys/__assert.h>
+#include <string.h>
+
+/* size of stack area used by each thread */
+#define NOTHING_STACKSIZE 2048
+
+/* scheduling priority used by each thread */
+#define NOTHING_PRIORITY 5
+
+void nothing(void)
+{
+	while (1) {}
+}
+
+K_THREAD_DEFINE(nothing_tid, NOTHING_STACKSIZE, nothing, NULL, NULL, NULL,
+ 		NOTHING_PRIORITY, 0, 0);
