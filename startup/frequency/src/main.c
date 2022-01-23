9440bc8649ac3cd52b6f966ab76fdd1bee99b00e
Daniel Schultz <d.schultz@phytec.de>
2022-01-23 21:50:12 +0100

    Simplify headers
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- startup/frequency/src/main.c
+++ startup/frequency/src/main.c
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2021 Phytec Messtechnik GmbH
- *
  * SPDX-License-Identifier: Apache-2.0
  */
 
--- startup/uart/src/main.c
+++ startup/uart/src/main.c
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2020 Phytec Messtechnik GmbH
- *
  * SPDX-License-Identifier: Apache-2.0
  */
 
