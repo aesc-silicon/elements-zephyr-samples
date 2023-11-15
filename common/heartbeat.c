c6a50ba5ee4e38ff2eaef3a171899c1f3cb36d43
Daniel Schultz <d.schultz@phytec.de>
2023-11-15 21:54:52 +0100

    common: heartbeat: Add error messages
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- common/heartbeat.c
+++ common/heartbeat.c
@@ -14,35 +14,41 @@ void heartbeat(void) {
 	int ret;
 
 	if (!device_is_ready(led.port)) {
+		printk("LED device is not ready!");
 		return;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
+		printk("Failed to configure LED pin: %d!", ret);
 		return;
 	}
 
 	while (1) {
 		ret = gpio_pin_set_dt(&led, 1);
 		if (ret < 0) {
+			printk("Failed to turn LED on: %d", ret);
 			return;
 		}
 		k_sleep(K_MSEC(150));
 
 		ret = gpio_pin_set_dt(&led, 0);
 		if (ret < 0) {
+			printk("Failed to turn LED off: %d", ret);
 			return;
 		}
 		k_sleep(K_MSEC(50));
 
 		ret = gpio_pin_set_dt(&led, 1);
 		if (ret < 0) {
+			printk("Failed to turn LED on: %d", ret);
 			return;
 		}
 		k_sleep(K_MSEC(150));
 
 		ret = gpio_pin_set_dt(&led, 0);
 		if (ret < 0) {
+			printk("Failed to turn LED off: %d", ret);
 			return;
 		}
 		k_sleep(K_SECONDS(1));
